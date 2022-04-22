72485afbbf7e3fbd5f1d9f097dabb4a1da197f3c
HuyNT42 <nguyenthaihuy2802@gmail.com>
2022-04-22 09:23:55 +0700

    search

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -25,16 +25,16 @@ const Header = () => {
     <div className='container-fluid'>
       <nav className='navbar navbar-expand-lg navbar-light bg-light px-3'>
         <div className='container-fluid'>
-          <a className='navbar-brand' href='#'>
+          <a className='navbar-brand' href='/'>
             Home
           </a>
           <div className='collapse navbar-collapse' id='navbarSupportedContent'>
             <ul className='navbar-nav me-auto mb-2 mb-lg-0'>
               <li className='nav-item'>
-                <a className='nav-link'>Category Page</a>
+                <a className='nav-link' href='/'>Category Page</a>
               </li>
               <li className='nav-item'>
-                <a className='nav-link' href='#'>
+                <a className='nav-link' href='/'>
                   Link
                 </a>
               </li>
@@ -44,16 +44,17 @@ const Header = () => {
                 className='form-control me-2'
                 type='search'
                 placeholder='Search'
+                
               />
               <button className='btn btn-outline-success' type='submit'>
                 Search
               </button>
             </form>
             <form className='d-flex align-items-center'>
-              <a className='nav-link' href='#'>
+              <a className='nav-link' href='/'>
                 Login
               </a>
-              <a className='nav-link' href='#'>
+              <a className='nav-link' href='/'>
                 Logout
               </a>
               <ion-icon
